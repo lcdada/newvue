a49a2213e49900cd6515a354ddc03e79dfa30424
lcdada <15810227932@163.com>
2019-05-10 17:10:37 +0800

    chooose_center

---
+++ src/page/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="list">
+			<ul>
+				<li class="item">A</li>
+				<li class="item">A</li>
+				<li class="item">A</li>
+				<li class="item">A</li>
+				<li class="item">A</li>
+				<li class="item">A</li>
+				<li class="item">A</li>
+			</ul>
+    </div>
+</template>
+<script>
+export default {
+  name: "CityAlphabet"
+};
+</script>
+<style lang="stylus" scoped>
+	@import '../../../assets/styles/varibles.styl'
+	.list
+		display flex 
+		flex-direction column
+		justify-content center
+		position absolute
+		top 1.58rem
+		right 0
+		bottom 0
+		width .4rem 
+		.item
+			line-height .44rem
+			text-align center
+</style>
---
+++ src/page/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="search">
+        <input class="search-input" type = "text" placeholder="输入城市名字">
+    </div>
+</template>
+<script>
+export default {
+  name: "CitySearch"
+}
+</script>
+<style lang="stylus" scoped>
+    @import '../../../assets/styles/varibles.styl'
+    .search
+        height .7rem
+        background $bgColor
+        padding .1rem
+        .search-input
+            box-sizing border-box
+            width 100%
+            height .62rem
+            line-height .62rem
+            text-align center
+            border-raduis .06rem
+            color #666
+            padding 0 .1rem
+</style>
