bba4034dc09716f015e9ec0e88e2052ce2cea951
lcdada <15810227932@163.com>
2019-05-13 18:09:06 +0800

    vuex

--- src/page/city/components/Search.vue
+++ src/page/city/components/Search.vue
@@ -1,26 +1,184 @@
+// <template>
+//     <div>
+//         <div class="search">
+//             <input v-model="keyword" class="search-input" type = "text" placeholder="输入城市名字">
+//         </div>
+//         <div class="search-content">
+//             <ul>
+//                 <li v-for="item of list">{{item}}</li>
+//             </ul>
+//         </div>
+//     </div>
+// </template>
+// <script>
+// export default {
+//   name: "CitySearch",
+//   props:{
+//       cities:Object
+//   },
+//   data (){
+//       return {
+//           keyword :'',
+//           list:[], 
+//           timer:null
+//       }
+//   },
+//   watch:{
+//       keyword () {
+//           if (this.timer) {
+//               clearTimeout(this.timer)
+//           }
+//           this.timer= setTimeout(() => {
+//               const result = []
+//               for (let i in this.cities) {
+//                   this.cities[i].forEach((value) => {
+//                       if(value.spell.indexOf(this.keywoed) > -1 || value.name.indexOf(this.keyword) >-1){
+//                           result.push(value)
+//                       }
+//                   })
+//               }
+//               this.list = result
+//           },100)
+//       }
+//   }
+// }
+// </script>
+// <style lang="stylus" scoped>
+//     @import '../../../assets/styles/varibles.styl'
+//     .search
+//         height .7rem
+//         background $bgColor
+//         padding .1rem
+//         .search-input
+//             box-sizing border-box
+//             width 100%
+//             height .62rem
+//             line-height .62rem
+//             text-align center
+//             border-raduis .06rem
+//             color #666
+//             padding 0 .1rem
+//     .search-content
+//         z-index 1
+//         overflow hidden
+//         position absolute
+//         top 1.58rem
+//         left 0
+//         right 0
+//         bottom 0
+//         background green
+// </style>
+
+
 <template>
+  <div>
     <div class="search">
-        <input class="search-input" type = "text" placeholder="输入城市名字">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" />
     </div>
+    <div
+      class="search-content"
+      ref="search"
+      v-show="keyword"
+    >
+      <ul>
+        <li
+          class="search-item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          @click="handleCityClick(item.name)"
+        >
+          {{item.name}}
+        </li>
+        <li class="search-item border-bottom" v-show="hasNoData">
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
 <script>
+import Bscroll from 'better-scroll'
+// import { mapMutations } from 'vuex'
 export default {
-  name: "CitySearch"
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+//   methods: {
+//     handleCityClick (city) {
+//       this.changeCity(city)
+//       this.$router.push('/')
+//     },
+//     ...mapMutations(['changeCity'])
+//   },
+//   mounted () {
+//     this.scroll = new Bscroll(this.$refs.search)
+//   }
 }
 </script>
+
 <style lang="stylus" scoped>
-    @import '../../../assets/styles/varibles.styl'
-    .search
-        height .7rem
-        background $bgColor
-        padding .1rem
-        .search-input
-            box-sizing border-box
-            width 100%
-            height .62rem
-            line-height .62rem
-            text-align center
-            border-raduis .06rem
-            color #666
-            padding 0 .1rem
+  @import '../../../assets/styles/varibles.styl'
+  .search
+    height: .72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: .62rem
+      padding-left: .2rem
+      background: #fff
+      color: #666
 </style>
