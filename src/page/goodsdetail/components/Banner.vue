413d024c9f111c7b3b8a87f9745857dfd9e7f98a
lcdada <15810227932@163.com>
2019-07-17 13:58:05 +0800

    bannerheight

--- src/page/goodsdetail/components/Banner.vue
+++ src/page/goodsdetail/components/Banner.vue
@@ -29,8 +29,9 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
+    .Swiper_cont
+        height min-content
         .swiper_item_img
             width 100%
-            height 100%
             display block
 </style>
